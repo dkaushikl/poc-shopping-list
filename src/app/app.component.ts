0820e50f3916f6a30e382a831f675c12b0700270
Sema García <jose-manuel.garcia@gft.com>
2018-01-22 01:04:16 +0100

    Add page menu entries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginPage } from '../pages/login/login';
 import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { ShoppingListsPage } from '../pages/shopping-lists/shopping-lists';
+//import {} from '';
 
 @Component({
   templateUrl: 'app.component.html'
@@ -21,7 +24,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage }
+      { title: 'Home', component: HomePage },
+      { title: 'Shopping lists', component: ShoppingListsPage }
     ];
 
   }
