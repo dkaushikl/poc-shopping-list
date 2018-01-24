f9fec6c906311c5a1dacaf14a87ebf1df41406c0
Sema García <jose-manuel.garcia@gft.com>
2018-01-24 23:46:45 +0100

    Renamed service

--- src/providers/authentication/authentication.ts
+++ src/providers/authentication/authentication.ts
@@ -5,7 +5,7 @@ import * as firebase from 'firebase/app';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
-export class AuthServiceProvider {
+export class AuthenticationProvider {
   private user: Observable<firebase.User>;
 
   constructor(private firebaseAuthSrv: AngularFireAuth) {
