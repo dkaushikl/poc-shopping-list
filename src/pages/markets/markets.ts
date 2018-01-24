e768adad70bfe42df5b6fe371069093c93137d63
Sema García <jose-manuel.garcia@gft.com>
2018-01-25 00:15:55 +0100

    Links component method to service ones

--- src/pages/markets/markets.ts
+++ src/pages/markets/markets.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { MarketsProvider } from './../../providers';
+
 @IonicPage()
 @Component({
   selector: 'page-markets',
@@ -10,15 +12,28 @@ export class MarketsPage {
 
   markets: any[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private marketSrv: MarketsProvider) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MarketsPage');
+    this.marketSrv.getMarkets();
     this.markets = [
       { name: 'Mercadona', color: '#00FF00' },
       { name: 'Lidl', color: '#0000FF' },
       { name: 'Dia', color: '#FF0000' }
     ];
+  }
+
+  addMarket() {
+    this.marketSrv.addMarket();
+  }
+
+  editMarket() {
+    this.marketSrv.editMarket();
+  }
+
+  deleteMarket() {
+    this.marketSrv.deleteMarket();
   }
 
 }
