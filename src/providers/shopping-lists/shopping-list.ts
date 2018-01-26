c0263726d02ea0f0856ec714ef092f7fee611674
Sema García <jose-manuel.garcia@gft.com>
2018-01-26 23:33:52 +0100

    Test to create a new document with custom ID

--- src/providers/shopping-lists/shopping-list.ts
+++ src/providers/shopping-lists/shopping-list.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Injectable()
 export class ShoppingListProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(private afs: AngularFirestore) {
     console.log('Hello ShoppingListProvider Provider');
   }
 
   getAllLists() {
-
+    //this.afs.doc('').
   }
 
   getSharedLists() {
 
   }
 
-  createNewList() {
-
+  createNewList(listName: string, shared: boolean) {
+    return this.afs.collection('shopping-list-db')
+      .doc(listName)
+      .set({ name: listName, shared });
   }
 
   editList() {
