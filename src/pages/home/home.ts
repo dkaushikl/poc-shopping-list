f7413385d7eb6e07cd40d4bc3acb256b8a55e0ae
Sema García <jose-manuel.garcia@gft.com>
2018-01-26 23:35:26 +0100

    Initial approach to home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, ItemSliding, NavController } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppingLists: any[];
 
+  constructor(public navCtrl: NavController) { }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+    this.shoppingLists = [
+      { name: 'Compra semanal', checked: 3, total: 7, shared: false },
+      { name: 'Barbacoa Villa Lázaro', checked: 2, total: 9, shared: false }
+    ];
+  }
+
+  tap(itemTapped: ItemSliding) {
+    console.log('tap: ', itemTapped);
+  }
+
+  press(itemPressed: ItemSliding) {
+    console.log('press: ', itemPressed);
   }
 
 }
