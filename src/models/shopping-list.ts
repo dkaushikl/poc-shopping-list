07ea106f78d463d1cad7dc5b5b2e8e34660983f2
Sema García <jose-manuel.garcia@gft.com>
2018-01-26 23:32:46 +0100

    Adds models

---
+++ src/models/shopping-list.ts
@@ -0,0 +1,17 @@
+export class ShoppingList {
+    name: string;
+    description: string;
+    aliments: Array<AlimentItem>;
+    sharedWith: Array<string>;
+}
+
+export class AlimentItem {
+    name: string;
+    quantity: string;
+    market: string;
+}
+
+export class Market {
+    name: string;
+    color: string;
+}
