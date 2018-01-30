0a43177024218f90af2982879bd67d1b268720ca
Sema García <jose-manuel.garcia@gft.com>
2018-01-30 01:07:33 +0100

    Adds get and create operations for markets

--- src/providers/markets/markets.ts
+++ src/providers/markets/markets.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
+import { AngularFirestore } from 'angularfire2/firestore';
+
+import { Market } from './../../models';
+import { AuthenticationProvider } from './../authentication/authentication';
 
 @Injectable()
 export class MarketsProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello MarketsProvider Provider');
+  constructor(private authSrv: AuthenticationProvider, private afs: AngularFirestore) {
+    console.log('Hello MarketsProvider Provider -> ', this.authSrv.getCurrentUser().displayName);
   }
 
   getMarkets() {
-
+    return this.afs
+      .collection<Market>(`/shopping-list-db/${this.authSrv.getCurrentUserId()}/markets`)
+      .snapshotChanges()
+      .map(actions => {
+        return actions.map(action => {
+          const id = action.payload.doc.id;
+          const data = action.payload.doc.data() as Market;
+          return { id, ...data };
+        });
+      });
   }
 
-  addMarket() {
-
+  addMarket(name: string, color: string = null, location: string = null) {
+    return this.afs
+      .collection(`/shopping-list-db/${this.authSrv.getCurrentUserId()}/markets`)
+      .add({ name, color, location });
   }
 
   editMarket() {
