30312fe18bbc5086f707a6b7d8e68c80e5fd6115
Sema García <jose-manuel.garcia@gft.com>
2018-01-30 01:08:38 +0100

    Adds initial implementation for retrieving lists

--- src/pages/shopping-lists/shopping-lists.ts
+++ src/pages/shopping-lists/shopping-lists.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, ModalController, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, ItemSliding, ModalController, NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 
 import { AddNewListPage } from './../modals/';
 
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 
 @IonicPage()
 @Component({
@@ -12,32 +12,61 @@ import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/fires
   templateUrl: 'shopping-lists.html',
 })
 export class ShoppingListsPage {
+  private shoppingListCollection: AngularFirestoreCollection<any>;
+  private shoppingListDoc: AngularFirestoreDocument<any>;
+  shoppingLists: Observable<any[]>;
 
   constructor(
-    private db: AngularFirestore,
+    private afs: AngularFirestore,
     private modalCtrl: ModalController,
     public navCtrl: NavController
-  ) { }
+  ) { 
+    this.shoppingListDoc = this.afs.doc('shopping-list-db/foo');
+    this.shoppingListDoc.valueChanges().subscribe(
+      doc => console.log('>>> doc: ', doc),
+      error => console.log('Error: ', error)
+    );
+
+    this.shoppingListCollection = this.afs.collection<any>('shopping-list-db');
+    this.shoppingLists = this.shoppingListCollection.snapshotChanges()
+      .map(actions => {
+        return actions.map(action => ({
+          $key: action.payload.doc.id, 
+          ...action.payload.doc.data()
+        }));
+      });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ShoppingListsPage');
+    /*this.shoppingLists = [
+      { name: 'Compra semanal', checked: 3, total: 7, shared: false },
+      { name: 'Barbacoa Villa Lázaro', checked: 2, total: 9, shared: false }
+    ];*/
+    this.shoppingLists.subscribe(
+      (a) => console.log('a: ', a),
+      (e) => console.log('e: ', e) 
+    );
   }
 
   addShoppingList() {
-    console.log('Adding...');
-    /*this.navCtrl.push(AddNewListPage, {
-      animate: true,
-      direction: 'forward'
-    });*/
-
     this.modalCtrl.create(AddNewListPage).present();
+  }
 
-    /*this.userCollectionRef.add({
-      name: this.shoppingName,
-      timestamp: new Date()
-    })
-    .then(l => console.log('OK adding list: ', l))
-    .catch(e => console.log('Error adding shopping list'));*/
+  itemTapped(item: ItemSliding) {
+    console.log('tap: ', item);
+  }
+
+  itemPressed(item: ItemSliding) {
+    console.log('pressed: ', item);
+  }
+
+  editItem(item: ItemSliding) {
+    console.log('edit: ', item);
+  }
+
+  deleteItem(item: ItemSliding) {
+    console.log('deleting: ', item);
   }
 
 }
