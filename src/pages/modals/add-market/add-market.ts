c8910978a22adbc12ce21bd6d1f8254905499d1a
Sema García <jose-manuel.garcia@gft.com>
2018-01-30 01:11:00 +0100

    Adds implementation for creating markets

--- src/pages/modals/add-market/add-market.ts
+++ src/pages/modals/add-market/add-market.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+
+import { Market } from './../../../models';
+import { MarketsProvider, UtilProvider } from './../../../providers';
 
 @IonicPage()
 @Component({
@@ -7,12 +10,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'add-market.html',
 })
 export class AddMarketPage {
+  marketName: string;
+  marketColor: string;
+  marketLocation: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(private marketSrv: MarketsProvider, public navCtrl: NavController, private utilSrv: UtilProvider) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddMarketPage');
+  }
+
+  addNewMarket() {
+    this.marketSrv.addMarket(this.marketName, this.marketColor, this.marketLocation)
+      .then(docRef => {
+        console.log('docRef: ', docRef)
+        this.utilSrv.showToast('Market added successfully!');
+        this.navCtrl.pop();
+      })
+      .catch(error => this.utilSrv.showToast(error))
   }
 
   cancel() {
