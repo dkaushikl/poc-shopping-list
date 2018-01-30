7d65e68a50ad882bac936d315b2823c3b4968a9e
Sema García <jose-manuel.garcia@gft.com>
2018-01-30 01:07:51 +0100

    Adds get and create operations for shopping lists

--- src/providers/shopping-lists/shopping-list.ts
+++ src/providers/shopping-lists/shopping-list.ts
@@ -2,15 +2,27 @@ import { Injectable } from '@angular/core';
 
 import { AngularFirestore } from 'angularfire2/firestore';
 
+import { AuthenticationProvider } from './../authentication/authentication';
+
 @Injectable()
 export class ShoppingListProvider {
+  userId: string;
 
-  constructor(private afs: AngularFirestore) {
-    console.log('Hello ShoppingListProvider Provider');
+  constructor(private authSrv: AuthenticationProvider, private afs: AngularFirestore) {
+    console.log('Hello ShoppingListProvider Provider; id: ', this.authSrv.getCurrentUserId());
   }
 
   getAllLists() {
-    //this.afs.doc('').
+    return this.afs
+      .collection<any>(`/shopping-list-db/${this.authSrv.getCurrentUserId()}/lists`)
+      .snapshotChanges()
+      .map(actions => {
+        return actions.map(action => {
+          const id = action.payload.doc.id;
+          const data = action.payload.doc.data() as any;
+          return { id, ...data };
+        });
+      });
   }
 
   getSharedLists() {
@@ -18,9 +30,11 @@ export class ShoppingListProvider {
   }
 
   createNewList(listName: string, shared: boolean) {
-    return this.afs.collection('shopping-list-db')
-      .doc(listName)
-      .set({ name: listName, shared });
+    this.afs
+      .collection(`/shopping-list-db/${this.authSrv.getCurrentUserId()}/lists`)
+      .add({ name: listName, aliments: [], shared, sharedWith: [] })
+      .then(docRef => console.log('success: ', docRef))
+      .catch(error => console.log('error: ', error));
   }
 
   editList() {
