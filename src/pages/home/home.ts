512a834a1d6a86dd85bf877da4e8c6a0534cb311
Sema García <jose-manuel.garcia@gft.com>
2018-01-30 01:11:56 +0100

    Updates implementation for retrieving shopping lists

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, ItemSliding, NavController } from 'ionic-angular';
+import { IonicPage, ItemSliding, ModalController, NavController } from 'ionic-angular';
+
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+import { AddNewListPage } from './../modals';
+
+import { ShoppingListProvider } from './../../providers';
 
 @IonicPage()
 @Component({
@@ -10,14 +18,26 @@ export class HomePage {
 
   shoppingLists: any[];
 
-  constructor(public navCtrl: NavController) { }
+  constructor(
+      //private afAuth: AngularFireAuth, 
+      //private afs: AngularFirestore, 
+      private shoppingListSrv: ShoppingListProvider,
+      private modalCtrl: ModalController,
+      public navCtrl: NavController) { 
+    this.shoppingListSrv.getAllLists().subscribe(
+      (l) => {
+        this.shoppingLists = l;
+        console.log('Lists: ', l);
+      }
+    );
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
-    this.shoppingLists = [
+    /*this.shoppingLists = [
       { name: 'Compra semanal', checked: 3, total: 7, shared: false },
       { name: 'Barbacoa Villa Lázaro', checked: 2, total: 9, shared: false }
-    ];
+    ];*/
   }
 
   tap(itemTapped: ItemSliding) {
@@ -26,6 +46,10 @@ export class HomePage {
 
   press(itemPressed: ItemSliding) {
     console.log('press: ', itemPressed);
+  }
+
+  addShoppingList() {
+    this.modalCtrl.create(AddNewListPage).present();
   }
 
 }
