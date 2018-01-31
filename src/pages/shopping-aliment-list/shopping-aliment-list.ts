c1149e15344ed11a0d7905c3d2c41115f6ab337a
Sema García <jose-manuel.garcia@gft.com>
2018-01-31 03:39:13 +0100

    * Subscribes to the shopping list to listen aliment changes * Imports Aliments provider

--- src/pages/shopping-aliment-list/shopping-aliment-list.ts
+++ src/pages/shopping-aliment-list/shopping-aliment-list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, ModalController, NavController, NavParams, PopoverController } from 'ionic-angular';
 
-import { ShoppingList } from './../../models';
+import { AlimentItem, ShoppingList } from './../../models';
 import { AddAlimentPage } from './../modals';
 import { AlimentOptionsPage, FilteringOptionsPage } from './../popovers';
+import { AlimentsProvider } from './../../providers';
 import { ShoppingListProvider } from './../../providers';
 
 @IonicPage()
@@ -13,18 +14,22 @@ import { ShoppingListProvider } from './../../providers';
 })
 export class ShoppingAlimentListPage {
   shoppingList: ShoppingList;
+  listId: string;
 
   constructor(
+    private alimentSrv: AlimentsProvider,
     private modalCtrl: ModalController,
     public navCtrl: NavController, 
     public navParams: NavParams, 
     private popCtrl: PopoverController,
     private shoppingListSrv: ShoppingListProvider
   ) {
-    let listId = this.navParams.get('listId');
-    this.shoppingListSrv.getShoppingListById(listId)
-      .then(shoppingList => this.shoppingList = shoppingList.data() as ShoppingList)
-      .catch(e => console.log('error: ', e));
+    this.listId = this.navParams.get('listId');
+    this.shoppingListSrv.getShoppingListById(this.listId).subscribe(
+      shoppingLists => {
+        this.shoppingList = shoppingLists.payload.data() as ShoppingList;
+      }
+    );
   }
 
   ionViewDidLoad() {
@@ -38,7 +43,7 @@ export class ShoppingAlimentListPage {
   applyFilter(popoverEvent) {
     let popover = this.popCtrl.create(FilteringOptionsPage);
     popover.onDidDismiss(option => {
-      console.log('option: ', option);
+      console.log('filter: ', option);
     });
     popover.present({ ev: popoverEvent });
   }
@@ -53,8 +58,11 @@ export class ShoppingAlimentListPage {
 
   openModalToAddAliment() {
     let modal = this.modalCtrl.create(AddAlimentPage)
-    modal.onDidDismiss(data => {
+    modal.onDidDismiss((data: AlimentItem) => {
       console.log('data returned: ', data);
+      this.alimentSrv.addAliment(this.listId, data)
+        //.then(s => console.log('s: ', s))
+        //.catch(e => console.log('error: ', e))
     });
     modal.present();
   }
