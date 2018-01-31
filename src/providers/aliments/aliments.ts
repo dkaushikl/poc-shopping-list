b291b0f60b8df6f85e6170d8b1e6e8e5ac26962c
Sema García <jose-manuel.garcia@gft.com>
2018-01-31 03:32:16 +0100

    Adds aliments provider (service) and updates the barrel

---
+++ src/providers/aliments/aliments.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFirestore } from 'angularfire2/firestore';
+
+import { AlimentItem, ShoppingList } from './../../models';
+import { AuthenticationProvider } from './../authentication/authentication';
+
+@Injectable()
+export class AlimentsProvider {
+  userId: string;
+
+  constructor(private authSrv: AuthenticationProvider, private afs: AngularFirestore) {
+    console.log('Hello AlimentsProvider; id: ', this.authSrv.getCurrentUserId());
+  }
+
+  addAliment(listId: string, newAliment: AlimentItem) {
+    console.log('listId: ', listId);
+    const docRef = this.afs
+        .doc<any>(`/shopping-list-db/${this.authSrv.getCurrentUserId()}/lists/${listId}`)
+        .ref
+        .get()
+        .then(list => {
+          let aliments: Array<any> = list.get('aliments');
+          console.log('aliments: ', aliments);
+          return list.ref.update({ 'aliments': [...aliments, newAliment] })
+        })
+        .catch(e => console.log('e: ', e));
+        
+        //.set({ aliments: [aliment] }, { merge: true });
+  }
+
+}
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,3 +1,4 @@
+export * from './aliments/aliments';
 export * from './authentication/authentication';
 export * from './markets/markets';
 export * from './shopping-lists/shopping-list';
