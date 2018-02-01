50602a9ac79b8bba44deaa1f0ed31f38e7680cd8
Sema García <jose-manuel.garcia@gft.com>
2018-02-01 23:39:17 +0100

    Adds function headers and adds showAlert util method

--- src/providers/utils/util.ts
+++ src/providers/utils/util.ts
@@ -1,15 +1,38 @@
 import { Injectable } from '@angular/core';
-import { ToastController } from 'ionic-angular';
+import { AlertController, ToastController } from 'ionic-angular';
 
 @Injectable()
 export class UtilProvider {
 
-  constructor(private toastCtrl: ToastController) {
+  constructor(private alertCtrl: AlertController, private toastCtrl: ToastController) {
     console.log('Hello UtilProvider Provider');
   }
 
-  showToast(message: string, duration = 3000) {
-    this.toastCtrl.create({ message, duration }).present();
+  /**
+   * Displays toast message into the screen
+   * @param {string} message Message to display
+   */
+  public showToast(message: string, duration?: number, position?: string): void {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: duration || 3000,
+      position: position || 'bottom'
+    });
+    toast.present();
+  }
+
+  /**
+   * Prompts an alert into the screen
+   * @param {string} title Title of the alert
+   * @param {string} msg Message of the alert
+   * @param {Array<Object>} buttons (optional) Array of buttons. By default 'OK'
+   */
+  public showAlert(title: string, msg: string, buttons?: Array<Object>): void {
+    this.alertCtrl.create({
+      title: title,
+      subTitle: msg,
+      buttons: buttons || ['OK']
+    }).present();
   }
 
 }
