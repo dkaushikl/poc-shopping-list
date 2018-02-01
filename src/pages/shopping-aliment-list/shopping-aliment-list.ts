90921dcfc4bc7fac38ce9e0fb022d743554daf7c
Sema García <jose-manuel.garcia@gft.com>
2018-02-02 00:52:36 +0100

    * Adds delete method * Adds edit handler method * Adds more control for data returned by the modals * updates the response of the backend operation to show a toast with the result

--- src/pages/shopping-aliment-list/shopping-aliment-list.ts
+++ src/pages/shopping-aliment-list/shopping-aliment-list.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, ModalController, NavController, NavParams, PopoverController } from 'ionic-angular';
 
-import { AlimentItem, ShoppingList } from './../../models';
+import { AlimentItem, Market, ShoppingList } from './../../models';
 import { AddAlimentPage } from './../modals';
 import { AlimentOptionsPage, FilteringOptionsPage } from './../popovers';
-import { AlimentsProvider, ShoppingListProvider, UtilProvider } from './../../providers';
+import { AlimentsProvider, MarketsProvider, ShoppingListProvider, UtilProvider } from './../../providers';
 
 @IonicPage()
 @Component({
@@ -12,11 +12,14 @@ import { AlimentsProvider, ShoppingListProvider, UtilProvider } from './../../pr
   templateUrl: 'shopping-aliment-list.html',
 })
 export class ShoppingAlimentListPage {
+  marketsList: Array<Market>;
   shoppingList: ShoppingList;
+  takenAliments: Array<AlimentItem>;
   listId: string;
 
   constructor(
     private alimentSrv: AlimentsProvider,
+    private marketSrv: MarketsProvider,
     private modalCtrl: ModalController,
     public navCtrl: NavController, 
     public navParams: NavParams, 
@@ -25,15 +28,23 @@ export class ShoppingAlimentListPage {
     private utilSrv: UtilProvider
   ) {
     this.listId = this.navParams.get('listId');
+    this.takenAliments = new Array<AlimentItem>();
+    this.marketSrv.getMarkets().subscribe(
+      markets => this.marketsList = markets
+    );
     this.shoppingListSrv.getShoppingListById(this.listId).subscribe(
-      shoppingLists => {
-        this.shoppingList = shoppingLists.payload.data() as ShoppingList;
-      }
+      shoppingLists => this.shoppingList = shoppingLists.payload.data() as ShoppingList
     );
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ShoppingAlimentListPage');
+  }
+
+  getMarketColor(marketName: string) {
+    if(!this.marketsList) return '';
+    let market = this.marketsList.find(market => market.name === marketName);
+    return (market) ? market.color : '';
   }
 
   shareList() {
@@ -57,14 +68,47 @@ export class ShoppingAlimentListPage {
   }
 
   openModalToAddAliment() {
-    let modal = this.modalCtrl.create(AddAlimentPage)
+    let modal = this.modalCtrl.create(AddAlimentPage);
     modal.onDidDismiss((data: AlimentItem) => {
-      console.log('data returned: ', data);
       this.alimentSrv.addAliment(this.listId, data)
-        .then(alimentAdded => this.utilSrv.showToast(alimentAdded + ' added successfully!'))
-        .catch(e => console.log('error: ', e))
+        .then(alimentAdded => this.utilSrv.showToast(alimentAdded + ' has been added successfully!'))
+        .catch(e => this.utilSrv.showToast('Error: ' + e));
     });
     modal.present();
+  }
+
+  updateCheckedAlimentList(aliment: AlimentItem) {
+    // WIP
+  }
+
+  updateTakenAlimentList(aliment: AlimentItem) {
+    // WIP
+  }
+
+  editAlimentItem(alimentToEdit: AlimentItem) {
+    let modal = this.modalCtrl.create(AddAlimentPage, { alimentToEdit });
+    modal.onDidDismiss((modifiedAliment: AlimentItem) => {
+      this.alimentSrv.updateAliment(this.listId, alimentToEdit.name, modifiedAliment)
+        .then(alimentUpdated => this.utilSrv.showToast(alimentUpdated + ' has been updated successfully!'))
+        .catch(e => this.utilSrv.showToast('Error: ' + e));
+    });
+    modal.present();
+  }
+
+  deleteAlimentItem(aliment: AlimentItem) {
+    this.utilSrv.showAlert(
+      'Remove aliment', 
+      `Are you sure to delete "${aliment.name}"?`,
+      [
+        { text: 'Cancel', handler: () => {} },
+        { text: 'Remove', handler: () => {
+            this.alimentSrv.deleteAlimentFromShoppingList(this.listId, aliment)
+              .then(() => this.utilSrv.showToast(`"${aliment.name}" was removed successfully!`))
+              .catch(e => this.utilSrv.showToast('Error: ', e));
+          } 
+        }
+      ]
+    );
   }
 
 }
