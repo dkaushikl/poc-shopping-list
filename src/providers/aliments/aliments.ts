70a37cc650c31e02101d6a14900733f9307c98c6
Sema García <jose-manuel.garcia@gft.com>
2018-02-02 00:42:37 +0100

    * Adds delete aliment function * Adds update function * Adds add function

--- src/providers/aliments/aliments.ts
+++ src/providers/aliments/aliments.ts
@@ -13,20 +13,99 @@ export class AlimentsProvider {
     console.log('Hello AlimentsProvider; id: ', this.authSrv.getCurrentUserId());
   }
 
+  /**
+   * 
+   * @param listId 
+   * @param newAliment 
+   */
   addAliment(listId: string, newAliment: AlimentItem) {
     return new Promise((resolve, reject) => {
-      console.log('listId: ', listId);
-      const docRef = this.afs
+      this.afs
         .doc<any>(`/shopping-list-db/${this.authSrv.getCurrentUserId()}/lists/${listId}`)
         .ref
         .get()
         .then(list => {
-          let aliments: Array<any> = list.get('aliments') || [];
+          let aliments: Array<AlimentItem> = list.get('aliments') || [];
+
+          // Check if exists before to add!
+          if(aliments.find(a => a.name === newAliment.name)) {
+            return reject('The aliment already exists!');
+          }
+
+          // No exists, add the aliment
           return list.ref.update({ 'aliments': [...aliments, newAliment] })
             .then(() => resolve(newAliment.name))
-            .catch(e => reject(e));
+            .catch(e => {
+              console.log('Error updating the list with new aliment: ', e);
+              reject(e);
+            });
         })
-        .catch(e => console.log('e: ', e));
+        .catch(e => {
+          console.log('Error getting the list: ', e);
+          reject(e);
+        });
+    });
+  }
+
+  /**
+   * 
+   * @param listId 
+   * @param oldAliment 
+   * @param alimentToUpdate 
+   */
+  updateAliment(listId: string, oldAlimentName: string, alimentToUpdate: AlimentItem) {
+    return new Promise((resolve, reject) => {
+      this.afs
+        .doc<any>(`/shopping-list-db/${this.authSrv.getCurrentUserId()}/lists/${listId}`)
+        .ref
+        .get()
+        .then(list => {
+          let aliments: Array<AlimentItem> = list.get('aliments') || [];
+          let alimentIndex = aliments.findIndex((a) => a.name === oldAlimentName);
+          if(alimentIndex === -1) {
+            return reject('No aliment found :-(');
+          }
+          aliments[alimentIndex] = alimentToUpdate;
+          return list.ref.update({ 'aliments': [...aliments] })
+            .then(() => resolve(alimentToUpdate.name))
+            .catch(e => {
+              console.log('Error updating aliment: ', e);
+              reject(e);
+            });
+        })
+        .catch(e => {
+          console.log('Error updating the aliment: ', e);
+          reject(e);
+        });
+    });
+  }
+
+  /**
+   * 
+   * @param listId 
+   * @param aliment 
+   */
+  deleteAlimentFromShoppingList(listId: string, aliment: AlimentItem) {
+    return new Promise((resolve, reject) => {
+      this.afs
+        .doc<AlimentItem>(`/shopping-list-db/${this.authSrv.getCurrentUserId()}/lists/${listId}`)
+        .ref
+        .get()
+        .then(list => {
+          let aliments: Array<AlimentItem> = list.get('aliments') || [];
+          let alimentIndex = aliments.findIndex((a) => a.name === aliment.name);
+          aliments.splice(alimentIndex, 1);
+          return list.ref.update({ 'aliments': [...aliments] })
+            .then(() => resolve(aliment.name))
+            .catch(e => {
+              console.log('Error deleting the list with new aliment: ', e);
+              reject(e);
+            });
+        })
+        .catch(e => {
+          console.log('Error deleting the list: ', e);
+          reject(e);
+        });
     });
   }
 
