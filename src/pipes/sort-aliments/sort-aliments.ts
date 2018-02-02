969eea9b30b3e33262f046f1e767f4257a7571df
Sema García <jose-manuel.garcia@gft.com>
2018-02-02 04:37:43 +0100

    Implements sorting logic for shopping lists

--- src/pipes/sort-aliments/sort-aliments.ts
+++ src/pipes/sort-aliments/sort-aliments.ts
@@ -3,13 +3,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { AlimentItem, FilterCriteria } from './../../models';
 
 @Pipe({
-  name: 'sortAliments',
+  name: 'sortAliments'
 })
 export class SortAlimentsPipe implements PipeTransform {
 
-  transform(aliments: Array<AlimentItem>, filteringCriteria: FilterCriteria) {
-    console.log('Pipe value: ', aliments);
-    return aliments;
+  transform(aliments: Array<AlimentItem>, filteringCriteria: FilterCriteria) {    
+    switch(filteringCriteria.sorting) {
+      case 'asc':
+        return aliments.sort(this.stringFunctionComparison);
+      case 'desc':
+        return aliments.sort((a, b) => this.stringFunctionComparison(a, b) * -1);
+      case 'none':
+      default: return aliments;
+    }
+  }
+
+  private stringFunctionComparison(alimentA: AlimentItem, alimentB: AlimentItem) {
+    return (alimentA.name === alimentB.name) 
+      ? 0
+      : (alimentA.name > alimentB.name) ? 1 : -1;
   }
 
 }
