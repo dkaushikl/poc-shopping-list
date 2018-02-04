e83a2a39d380b69bd33012e542e1225c77f9e074
Sema García <jose-manuel.garcia@gft.com>
2018-02-04 23:30:19 +0100

    Updates popover to retain the last selected values for filtering

--- src/pages/popovers/filtering-options/filtering-options.ts
+++ src/pages/popovers/filtering-options/filtering-options.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
+import { FilterCriteria } from './../../../models';
+
 @IonicPage()
 @Component({
   selector: 'page-filtering-options',
@@ -10,12 +12,14 @@ export class FilteringOptionsPage {
   itemsVisibility: string;
   sortingCriteria: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+    let currentValues = this.navParams.get('currentValues') as FilterCriteria;
+    this.itemsVisibility = currentValues.visibility || 'show';
+    this.sortingCriteria = currentValues.sorting || 'none';
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FilteringOptionsPage');
-    this.itemsVisibility = 'show';
-    this.sortingCriteria = 'none';
   }
 
   close() {
