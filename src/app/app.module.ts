fab57451b70240adc8c6c367516906c9dd517e14
Sema García <jose-manuel.garcia@gft.com>
2018-02-05 00:51:53 +0100

    Adds a barrel for importing all modules at once

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,26 +9,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { firebaseConfig } from './../config';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFirestoreModule, AngularFirestore } from 'angularfire2/firestore';
-import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
-// Pages
-import { MyApp } from './app.component';
-import { LoginPage } from '../pages/login/login';
-import { HomePage } from '../pages/home/home';
-import { MarketsPage } from '../pages/markets/markets';
-import { ShoppingListsPage } from '../pages/shopping-lists/shopping-lists';
-import { ShoppingAlimentListPage } from '../pages/shopping-aliment-list/shopping-aliment-list';
-import { ConfigurationPage } from '../pages/configuration/configuration';
-import { ProfilePage } from '../pages/profile/profile';
-import { AboutPage } from '../pages/about/about';
-
-// Modals & Popovers
+// Modules
+import { ALL_PAGE_MODULES } from './../pages';
 import { ALL_MODAL_MODULES } from './../pages/modals';
 import { ALL_POPOVER_MODULES } from './../pages/popovers';
 
-// Pipes
-import { ALL_PIPES } from './../pipes';
+// Pages
+import { MyApp } from './app.component';
 
 // Providers
 import { 
@@ -37,50 +26,33 @@ import {
 
 @NgModule({
   declarations: [
-    MyApp,
-    LoginPage,
-    HomePage,
-    MarketsPage,
-    ShoppingListsPage,
-    ShoppingAlimentListPage,
-    ConfigurationPage,
-    ProfilePage,
-    AboutPage,
-    ...ALL_PIPES
+    MyApp
   ],
   imports: [
-    ...ALL_MODAL_MODULES,
-    ...ALL_POPOVER_MODULES,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    AngularFireDatabaseModule,
     BrowserModule,
+    ...ALL_PAGE_MODULES,
+    ...ALL_MODAL_MODULES,
+    ...ALL_POPOVER_MODULES,
     HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    LoginPage,
-    HomePage,
-    MarketsPage,
-    ShoppingListsPage,
-    ShoppingAlimentListPage,
-    ConfigurationPage,
-    ProfilePage,
-    AboutPage
+    MyApp
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
     AlimentsProvider,
     AuthenticationProvider,
     MarketsProvider,
     ShoppingListProvider,
-    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    SplashScreen,
+    StatusBar,
     UtilProvider,
     UsersProvider,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
---
+++ src/pages/index.ts
@@ -0,0 +1,19 @@
+import { AboutPageModule } from '../pages/about/about.module';
+import { ConfigurationPageModule } from '../pages/configuration/configuration.module';
+import { HomePageModule } from '../pages/home/home.module';
+import { MarketsPageModule } from '../pages/markets/markets.module';
+import { LoginPageModule } from '../pages/login/login.module';
+import { ShoppingAlimentListPageModule } from '../pages/shopping-aliment-list/shopping-aliment-list.module';
+import { ShoppingListsPageModule } from '../pages/shopping-lists/shopping-lists.module';
+import { ProfilePageModule } from '../pages/profile/profile.module';
+
+export const ALL_PAGE_MODULES = [
+    AboutPageModule,
+    ConfigurationPageModule,
+    HomePageModule,
+    MarketsPageModule,
+    LoginPageModule,
+    ShoppingAlimentListPageModule,
+    ShoppingListsPageModule,
+    ProfilePageModule
+];
