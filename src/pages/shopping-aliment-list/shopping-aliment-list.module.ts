9b315aecc7bcccdccad749a740798c56721ab8d8
Sema García <jose-manuel.garcia@gft.com>
2018-02-05 00:52:22 +0100

    Adds import for SortAliments pipe

--- src/pages/shopping-aliment-list/shopping-aliment-list.module.ts
+++ src/pages/shopping-aliment-list/shopping-aliment-list.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 
+import { SortAlimentsPipe } from './../../pipes/sort-aliments/sort-aliments';
+
 import { ShoppingAlimentListPage } from './shopping-aliment-list';
 
 @NgModule({
   declarations: [
     ShoppingAlimentListPage,
+    SortAlimentsPipe
   ],
   imports: [
     IonicPageModule.forChild(ShoppingAlimentListPage)
