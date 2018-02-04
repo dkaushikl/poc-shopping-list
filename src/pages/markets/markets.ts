57cb9ceef8e1d990ef1def925dcd14f52767a20e
Sema García <jose-manuel.garcia@gft.com>
2018-02-04 23:38:01 +0100

    Implements CRUD operations for markets

--- src/pages/markets/markets.ts
+++ src/pages/markets/markets.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, ModalController, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, ItemSliding, ModalController, NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 
 import { Market } from './../../models';
 import { AddMarketPage } from './../modals';
-import { MarketsProvider } from './../../providers';
+import { MarketsProvider, UtilProvider } from './../../providers';
 
 @IonicPage()
 @Component({
@@ -12,30 +12,61 @@ import { MarketsProvider } from './../../providers';
   templateUrl: 'markets.html',
 })
 export class MarketsPage {
-
-  marketsObs: Observable<Array<Market>>;
+  markets = new Array<Market>();
 
   constructor(
     private marketSrv: MarketsProvider,
     private modalCtrl: ModalController,
     public navCtrl: NavController, 
-    public navParams: NavParams) {}
+    public navParams: NavParams,
+    private utilSrv: UtilProvider) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MarketsPage');
-    this.marketsObs = this.marketSrv.getMarkets();
+    this.marketSrv.getMarkets().subscribe(
+      docSnapshot => {
+        let marketsObj = docSnapshot.payload.data();
+        this.markets = Object.keys(marketsObj).map(market => marketsObj[market]);
+      }
+    );
   }
 
   addMarket() {
-    this.modalCtrl.create(AddMarketPage).present();
+    let modal = this.modalCtrl.create(AddMarketPage);
+    modal.onDidDismiss((data: Market) => {
+      this.marketSrv.addMarket(data.name, data.color, data.location)
+        .then(() => this.utilSrv.showToast('Market added successfully!'))
+        .catch(e => this.utilSrv.showToast('Error: ' + e));
+    });
+    modal.present();
   }
 
-  editMarket() {
-    this.marketSrv.editMarket();
+  editMarket(market: Market) {
+    let modal = this.modalCtrl.create(AddMarketPage, { market });
+    modal.onDidDismiss((data: Market) => {
+      this.marketSrv.editMarket(data)
+        .then(() => this.utilSrv.showToast(`Market ${market.name} edited successfully!`))
+        .catch(e => this.utilSrv.showToast('Error: ' + e));
+    });
+    modal.present();
   }
 
-  deleteMarket() {
-    this.marketSrv.deleteMarket();
+  deleteMarket(slidingItem: ItemSliding, market: Market) {
+    this.utilSrv.showAlert(
+      'Remove market', 
+      `Are you sure to delete "${market.name}"?`,
+      [
+        { text: 'Cancel', handler: () => slidingItem.close() },
+        { text: 'Remove', handler: () => {
+          this.marketSrv.deleteMarket(market.name)
+            .then(() => {
+              slidingItem.close();
+              this.utilSrv.showToast(`"${market.name}" was removed successfully!`);
+            })
+            .catch(e => this.utilSrv.showToast('Error: ', e));
+          } 
+        }
+      ]
+    );
   }
 
 }
