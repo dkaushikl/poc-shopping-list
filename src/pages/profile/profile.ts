72bf2b5b7baec2e037d39d20a5df91b9fba1bef6
Sema García <jose-manuel.garcia@gft.com>
2018-02-05 01:21:09 +0100

    Adds profile picture load

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { AuthenticationProvider } from './../../providers';
 
 @IonicPage()
 @Component({
@@ -14,12 +9,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
+  userProfilePhoto: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private authSrv: AuthenticationProvider, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
+    console.log('ionViewDidLoad ProfilePage: ', this.authSrv.getCurrentUser().photoURL);
+    this.userProfilePhoto = this.authSrv.getCurrentUser().photoURL;
   }
 
 }
