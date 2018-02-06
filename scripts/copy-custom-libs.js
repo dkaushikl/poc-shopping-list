d16380b131c126cefae0184ec1a8d78053c8be51
Sema García <jose-manuel.garcia@gft.com>
2018-02-06 06:35:34 +0100

    Adds custom script to copy libs (font-awesome)

---
+++ scripts/copy-custom-libs.js
@@ -0,0 +1,14 @@
+// Added font-awesome as custom script
+const existingConfig = require('../node_modules/@ionic/app-scripts/config/copy.config');
+
+module.exports = Object.assign(existingConfig, {
+    copyFontawesomeFonts: {
+      src: ['./node_modules/font-awesome/fonts/**/*'],
+      dest: './www/assets/fonts'
+    },
+    copyFontawesomeCss: {
+      src: ['./node_modules/font-awesome/css/font-awesome.min.css'],
+      dest: './www/assets/css'
+    }
+  }
+);
