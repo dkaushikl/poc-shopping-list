bd55ba30589eb504e37559171774122914402e99
Sema García <jose-manuel.garcia@gft.com>
2018-02-08 04:12:41 +0100

    Prepares the methods for sending invitations

--- src/pages/modals/add-new-list/add-new-list.ts
+++ src/pages/modals/add-new-list/add-new-list.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
-import { ShoppingListProvider, UtilProvider } from './../../../providers';
+import { ShoppingList } from './../../../models';
+import { ShoppingListProvider, UsersProvider, UtilProvider } from './../../../providers';
 
 @IonicPage()
 @Component({
@@ -10,32 +11,57 @@ import { ShoppingListProvider, UtilProvider } from './../../../providers';
 })
 export class AddNewListPage {
 
+  userInvitations: Array<string>;
   shoppingListName: string;
+  editMode: boolean;
   sharedList: boolean;
 
   constructor(
     private navCtrl: NavController, 
+    private navParams: NavParams,
     private shoppingListSrv: ShoppingListProvider,
-    private utilSrv: UtilProvider
-  ) { }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddNewListPage');
-    this.shoppingListName = '';
-    this.sharedList = false;
+    private userSrv: UsersProvider,
+    private utilSrv: UtilProvider,
+    private viewCtrl: ViewController
+  ) { 
+    let params: ShoppingList = this.navParams.get('listToEdit');
+    this.userInvitations = new Array<string>();
+    if(params) {
+      this.editMode = true;
+      this.shoppingListName = params.name;
+      this.sharedList = Object.keys(params.sharedWith || {}).length > 1;
+    } else {
+      this.editMode = false;
+      this.shoppingListName = '';
+      this.sharedList = false;
+    }
   }
 
-  shareShoppingListWithUser() {
-    
+  ionViewDidLoad() { }
+
+  prepareNewInvitation() {
+    this.userInvitations.push('');
+  }
+
+  removeInvitation(index: number) {
+    this.userInvitations.splice(index, 1);
   }
 
   createNewShoppingList() {
+    let invitations = this.userInvitations.filter((inv) => this.userSrv.isValidEmail(inv));
+    console.log('inv: ', invitations);
     this.shoppingListSrv.createNewList(this.shoppingListName, this.sharedList)
       .then(docRef => {
         this.utilSrv.showToast('List created successfully');
         this.navCtrl.pop();
       })
       .catch(error => console.log('error: ', error));
+  }
+
+  editShoppingList() {
+    this.viewCtrl.dismiss({
+      name: this.shoppingListName
+    });
   }
 
   cancel() {
