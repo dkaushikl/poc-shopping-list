1d3e7d4c93409a5320cdf2bf049b13badbbbc1cf
Sema García <jose-manuel.garcia@gft.com>
2018-02-17 04:49:01 +0100

    Adds service to receive messages from FCM

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,6 +2,7 @@ export * from './aliments/aliments';
 export * from './authentication/authentication';
 export * from './camera/camera';
 export * from './markets/markets';
+export * from './messaging/messaging';
 export * from './shopping-lists/shopping-list';
 export * from './users/users';
 export * from './utils/util';
---
+++ src/providers/messaging/messaging.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+import 'rxjs/add/operator/take';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
+
+import * as firebase from 'firebase';
+//import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+import { AuthenticationProvider } from './../authentication/authentication';
+import { UtilProvider } from './../utils/util';
+
+@Injectable()
+export class MessagingProvider {
+  messaging = firebase.messaging();
+  messages$ = new BehaviorSubject(null);
+
+  constructor(private afs: AngularFirestore, private authSrv: AuthenticationProvider, private utilSrv: UtilProvider) {
+    console.log('Hello MessagingProvider Provider');
+  }
+
+  updateToken(token) {
+    this.authSrv.getUserObservable().subscribe(
+      (auth) => (auth && auth.uid) 
+        ? this.afs.doc(`/fcmTokens/${this.authSrv.getCurrentUserId()}`).set({ token })
+        : null 
+    );
+  }
+
+  getPermission() {
+    this.messaging.requestPermission()
+      .then(() => {
+        return this.messaging.getToken();
+      })
+      .then((token) => {
+        console.log('TokeN: ', token);
+        this.updateToken(token);
+      })
+      .catch(error => this.utilSrv.showToast(`Error: Unable to get permission to notify (${error})`));
+  }
+
+  receiveMessage() {
+    this.messaging.onMessage((payload) => {
+      console.log("Message received. ", payload);
+      this.messages$.next(payload);
+    });
+  }
+
+}
