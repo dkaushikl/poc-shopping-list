893b4e2e0660111cebeed22038c9eebf0fc788f9
Sema García <jose-manuel.garcia@gft.com>
2018-02-17 04:43:35 +0100

    Adds ServiceWorker to register messages

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+// These scripts are made available when the app is served or deployed on Firebase Hosting
+// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
+/*
+importScripts('/__/firebase/4.8.1/firebase-app.js');
+importScripts('/__/firebase/4.8.1/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
+*/
+
+
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+firebase.initializeApp({
+  'messagingSenderId': '1064254593127'
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler((payload) => {
+  console.log('[Firebase SW] Received message... ', payload);
+
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message Body....',
+    icon: 'https://www.shareicon.net/download/2016/07/08/117548_google_512x512.png'
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
