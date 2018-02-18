725a5af7ed28fa972e31e31b05651f25d243a0c9
Sema García <jose-manuel.garcia@gft.com>
2018-02-18 21:19:16 +0100

    feat (pipes): creates new pipe to display the size of the attached files

---
+++ src/pipes/file-size/file-size.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { AlimentItem, FilterCriteria } from './../../models';
+
+@Pipe({
+  name: 'fileSize'
+})
+export class FileSizePipe implements PipeTransform {
+
+    transform(fileSize: number, outputSize: string) {
+        if(!outputSize) {  // default option: in bytes
+            return fileSize;
+        } else if(outputSize.toLowerCase() === 'kb') {
+            return (fileSize / 1024).toFixed(2);
+        } else if(outputSize.toLowerCase() === 'mb') {
+            return (fileSize / (1024 * 1024)).toFixed(2);
+        } else {
+            return fileSize;
+        }
+    }
+
+}
--- src/pipes/index.ts
+++ src/pipes/index.ts
@@ -1,5 +1,7 @@
+import { FileSizePipe } from './file-size/file-size';
 import { SortAlimentsPipe } from './sort-aliments/sort-aliments';
 
 export const ALL_PIPES = [
+    FileSizePipe,
     SortAlimentsPipe
 ];
