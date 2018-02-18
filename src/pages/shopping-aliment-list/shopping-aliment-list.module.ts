a61875928cf0fe59cb7848742a24e9d89c3e1f4b
Sema García <jose-manuel.garcia@gft.com>
2018-02-18 21:20:49 +0100

    feat (lists): imports the fileSize pipe to put it available for the view

--- src/pages/shopping-aliment-list/shopping-aliment-list.module.ts
+++ src/pages/shopping-aliment-list/shopping-aliment-list.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-
 import { Camera, CameraOptions } from '@ionic-native/camera';
 
 import { IonicImageViewerModule } from 'ionic-img-viewer';
 
 import { ShoppingAlimentListPage } from './shopping-aliment-list';
-import { SortAlimentsPipe } from './../../pipes/sort-aliments/sort-aliments';
+
+import { ALL_PIPES } from './../../pipes';
 
 @NgModule({
   declarations: [
     ShoppingAlimentListPage,
-    SortAlimentsPipe
+    ...ALL_PIPES
   ],
   imports: [
     IonicImageViewerModule,
