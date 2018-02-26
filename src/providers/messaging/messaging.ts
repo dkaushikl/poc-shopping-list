f18789332328ffb649e981916438bde9af45b773
Sema García <jose-manuel.garcia@gft.com>
2018-02-26 21:02:34 +0100

    feat (firebase token): updates implementation for updating token

--- src/providers/messaging/messaging.ts
+++ src/providers/messaging/messaging.ts
@@ -18,22 +18,40 @@ export class MessagingProvider {
     console.log('Hello MessagingProvider Provider');
   }
 
-  updateToken(token) {
-    this.authSrv.getUserObservable().subscribe(
-      (auth) => (auth && auth.uid) 
-        ? this.afs.doc(`/fcmTokens/${this.authSrv.getCurrentUserId()}`).set({ token })
-        : null 
-    );
+  updateToken(token: string) {
+    return new Promise((resolve, reject) => {
+      console.log('Update token over: ', this.authSrv.getCurrentUserId());
+      let docPath = `/fcmTokens/${this.authSrv.getCurrentUserId()}`;
+      this.afs.doc(docPath)
+        .ref.get()
+        .then(doc => {
+          let currentToken = {};
+          console.log('Am i on native? ', this.utilSrv.isNativePlatform());
+          if(doc.exists) {
+            console.log('Exists the doc: ', doc.data());
+            currentToken = doc.data();
+            currentToken = (this.utilSrv.isNativePlatform()) ? { native: token } : { web: token };
+            return this.afs.doc(docPath).update(currentToken);
+          } else {
+            console.log('No exists the doc');
+            currentToken = (this.utilSrv.isNativePlatform()) ? { native: token } : { web: token };
+            return this.afs.doc(docPath).set(currentToken);
+          }
+        })
+        .then((result) => resolve('T>> ' + JSON.stringify(result)))
+        .catch(err => reject(err));
+    });
   }
 
   getPermission() {
     this.messaging.requestPermission()
-      .then(() => {
+      .then((a) => {
+        console.log('Requested permissions: ', a);
         return this.messaging.getToken();
       })
       .then((token) => {
-        console.log('TokeN: ', token);
-        this.updateToken(token);
+        console.log('TokeN web: ', token);
+        return this.updateToken(token);
       })
       .catch(error => this.utilSrv.showToast(`Error: Unable to get permission to notify (${error})`));
   }
