525b1391f0c5e663046e314e65b947c7294705eb
Sema García <jose-manuel.garcia@gft.com>
2018-03-11 23:54:32 +0100

    Adds traces for debugging

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,19 +1,20 @@
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-/*
-importScripts('/__/firebase/4.8.1/firebase-app.js');
-importScripts('/__/firebase/4.8.1/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
-*/
 
+console.log('Firebase Messaging SW...');
 
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
 firebase.initializeApp({
   'messagingSenderId': '1064254593127'
 });
 
 const messaging = firebase.messaging();
+
+messaging.onMessage(payload => {
+  console.log('Message received!');
+});
 
 messaging.setBackgroundMessageHandler((payload) => {
   console.log('[Firebase SW] Received message... ', payload);
