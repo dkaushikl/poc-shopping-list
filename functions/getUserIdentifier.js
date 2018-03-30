f1f4d3162700e9b144735f242e374f536d63b057
Sema García <jose-manuel.garcia@gft.com>
2018-03-31 01:45:32 +0200

    Updated implementation for improving it

--- functions/getUserIdentifier.js
+++ functions/getUserIdentifier.js
@@ -8,9 +8,10 @@ const functions = require('firebase-functions');
 module.exports = functions.https.onRequest((req, res) => {    
     console.log('>> ', req.body.emails);
     
-    let email = 'igagajm@gmail.com';
-    admin.firestore()
-        .collection('users', (ref) => { return ref.where('email', '==', email) })
+    //let email = 'igagajm@gmail.com';
+    /*admin.firestore()
+        .collection('users')
+        .where('email', '==', email)
         .get()
         .then(user => {
             //console.log('>> Exists?: ', user.exists);
@@ -25,5 +26,24 @@ module.exports = functions.https.onRequest((req, res) => {
         .catch(e => {
             console.log('>> Resp Error: ', e);
             return res.status(200).send('Error! ' + JSON.stringify(e));
+        });*/
+    Promise.all(req.body.emails.map(email => {
+        console.log('Searching: ', email);
+        return admin.firestore().collection('users').where('email', '==', email).get();
+    }))
+        .then(responses => {
+            let processedResponses = {};
+            responses.forEach((query, index) => {
+                //console.log('Size ' + i + ': ', responses[i].size);
+                query.docs.forEach(doc => {
+                    //console.log(' ===> ', doc.data());
+                    processedResponses[req.body.emails[index]] = doc.data();
+                });
+            });
+            res.status(200).send(processedResponses);
+        })
+        .catch(error => {
+            console.log('Error: ', error);
+            res.status(200).send('Error');
         });
 });
